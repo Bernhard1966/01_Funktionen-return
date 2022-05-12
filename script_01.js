9eeb54ef8f2ef6ada6d87d8964ab503ef01a89fe
Bernhard <103026981+Bernhard1966@users.noreply.github.com>
2022-05-12 15:15:40 +0200

    ..stay in vegas!

--- script_01.js
+++ script_01.js
@@ -14,13 +14,13 @@ ausgabeNamen();
  // let    
 function ausgabeNamen() {
     let firstName = "bianca";
-    console.log("hallo " + firstName + "!");
+    console.log("hallo ausgabe 1" + firstName + "!");
    
     }
     ausgabeNamen2("Berhard");
     ausgabeNamen2("otto");
     ausgabeNamen2("Mohnnete");
-function ausgabeNamen2(firstName) {
+function ausgabeNamen2(firstName) { //..stay in vegas!
     
     console.log("hallo " + firstName + "!");
    
