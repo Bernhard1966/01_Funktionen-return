7a1a51124b43f62875e7193ba6b77b576953815a
Bernhard <103026981+Bernhard1966@users.noreply.github.com>
2022-05-12 14:40:45 +0200

    aufrufe

--- script_01.js
+++ script_01.js
@@ -1,6 +1,19 @@
-"use strict";
+"use strict"
 
 /***** Funktionen 01 *****/
+//call
+//test();
 
 // 1. Kapselung von Codeblöcken
 
+function test() {
+
+    console.log("grisabella");
+}
+ausgabeNamen();
+    
+function ausgabeNamen() {
+    console.log("hallo bernd");
+    console.log("hallo Karo");
+    console.log("hallo Mohanned");
+    }
